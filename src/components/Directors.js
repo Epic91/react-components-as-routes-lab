504eaee41cf0ff6f3486ece1fb26730e61655073
Evelyn Hernandez <hernandeze791@gmail.com>
2021-05-04 17:54:02 -0500

    Finished coding

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,20 +4,21 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      <h1>Director's Page</h1>
+      <h1>Directors Page</h1>
       {directors.map((director, index) => (
         <div key={index}>
-          <h3>Name: {director.name}</h3>
+          <h3>{director.name}</h3>
           <p>Movies:</p>
           <ul>
-            {director.movies.map((movie, index)=> (
+            {director.movies.map((movie, index) => (
               <li key={index}>{movie}</li>
             ))}
           </ul>
         </div>
-      ))}    
-      </div>
+      ))}
+
+    </div>
   );
 }
 
-export default Directors
+export default Directors;
